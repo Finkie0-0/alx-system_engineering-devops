4395f6aceb8c452a7d92b6e8922bed9b1d62bafd
Arnold Mdluli <arnoldmdluli00@gmail.com>
2023-02-01 01:33:51 +0000

    Task 0 fix 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-script of a function that queries the Reddit API and returns the number of subscribers
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
 import requests
 import sys
 
+
 def number_of_subscribers(subreddit):
     """ Queries to Reddit API """
-    user_agent = 'Mozilla/5.0'
+    u_agent = 'Mozilla/5.0'
 
     headers = {
-        "User-Agent": user_agent
+        'User-Agent': u_agent
     }
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -18,8 +20,8 @@ def number_of_subscribers(subreddit):
     if res.status_code != 200:
         return 0
     d = res.json()
-    if "data" not in d:
+    if 'data' not in d:
         return 0
-    if "subscribers" not in d.get('data'):
+    if 'subscribers' not in d.get('data'):
         return 0
     return res.json()['data']['subscribers']
